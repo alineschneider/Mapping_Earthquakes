a9976c8786c9bed4fc62edf75d3ec4a5204a8e2e
Aline Schneider <alinesviana@hotmail.com>
2020-06-06 21:56:34 -0400

    Updating logic.js multiple markers, varying sizes.

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,28 +4,61 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-//  Add a marker to the map for Los Angeles, California.
-L.circle([34.0522, -118.2437], {
-	color: "black",
-	fillColor: "yellow",
-	fillOpacity: 0.2,
-	radius: 300
- }).addTo(map);
 
-// Add a circle using circlemaker():
+
+//  Add a marker to the map for Los Angeles, California - radius in meters
+// L.circle([34.0522, -118.2437], {
+// 	color: "black",
+// 	fillColor: "yellow",
+// 	fillOpacity: 0.2,
+// 	radius: 300
+//  }).addTo(map);
+
+// Add a circle using circlemaker() - radius in pixels:
 //  L.circleMarker([34.0522, -118.2437], {
 // 	 color: "black",
 // 	 radius: 300,
 // 	 fillColor: "#ffffa1"
 //  }).addTo(map);
 
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+// 	console.log(city)
+// 	L.circleMarker(city.location, {
+// 		radius: city.population/100000
+// 	})
+// 	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   	.addTo(map);
+// });
+
+// SKILL DRILL
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+		radius: city.population/200000,
+		color: "orange",
+		fillColor: "orange",
+		fillOpacity: 0.2
+	})
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  	.addTo(map);
+});
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
+	maxZoom: 4,
 	id: "dark-v10",
 	accessToken: API_KEY
 });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//SKILL DRILL
+
+
